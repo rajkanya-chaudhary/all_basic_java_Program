325c96173ce0d8ffc42ac6d6a6a4acd57bc741bf
rajkanya-chaudhary <dc5068786@gmail.com>
2024-10-24 23:47:42 +0530

    this is a reverse number program using for loop

---
+++ Reverse.java
@@ -0,0 +1,13 @@
+public class Reverse {
+    public static void main(String[] args) {
+        int a = 4;
+        if(a/2==0)
+        {
+            System.out.println("Prime number");;
+        }
+        else
+        {
+            System.out.println("Not Prime number");
+        }
+    }
+}
